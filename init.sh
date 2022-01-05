f336856b0f99deb8ae3a83e9caecfbe1c2678ee3
Patrick M. Jensen <patmjen@dtu.dk>
2022-01-05 16:45:06 +0100

    Also load pandas module in init script

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 module load python3/3.8.11
 module load numpy/1.21.1-python-3.8.11-openblas-0.3.17
 module load scipy/1.6.3-python-3.8.11
+module load pandas/1.3.1-python-3.8.11
 module load cuda/11.3
 
 source /work1/patmjen/venvs/mdec/bin/activate
