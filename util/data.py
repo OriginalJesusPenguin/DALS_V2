b1ee0c38b45989f4239d2088cf60d2ad09150f0e
Patrick M. Jensen <patmjen@dtu.dk>
2022-01-17 14:56:13 +0100

    Add optional tqdm loadbar to mesh loding func.

--- util/data.py
+++ util/data.py
@@ -2,13 +2,17 @@ import os
 
 import pytorch3d.io
 
+from tqdm import tqdm
 
-def load_meshes_in_dir(path):
+
+def load_meshes_in_dir(path, loadbar=False):
     mesh_fnames = sorted(os.listdir(path))
     num_meshes = len(mesh_fnames)
 
     meshes = []
     io = pytorch3d.io.IO()
+    if loadbar:
+        mesh_fnames = tqdm(mesh_fnames)
     for fname in mesh_fnames:
         meshes.append(io.load_mesh(os.path.join(path, fname), 
                                    include_textures=False))
